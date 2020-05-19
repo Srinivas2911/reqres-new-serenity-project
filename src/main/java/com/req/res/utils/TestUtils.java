50908bf9d19ee0b3dd3f30c4ab7e392d02264cbb
sinivas2911 <srinivaspatnaik2911@gmail.com>
2020-05-19 22:56:31 +0100

    reqres-new-serenity-project

---
+++ src/main/java/com/req/res/utils/TestUtils.java
@@ -0,0 +1,16 @@
+package com.req.res.utils;
+
+/*
+Created by SP
+*/
+
+import java.util.Random;
+
+public class TestUtils {
+
+    public static String getRandomValue(){
+        Random random = new Random();
+        int randomInt = random.nextInt(100000);
+        return Integer.toString(randomInt);
+    }
+}
